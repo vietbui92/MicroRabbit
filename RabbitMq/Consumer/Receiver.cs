9fa0d566b9dd1ea5a9cf92143852d80e48c39395
Viet Bui <ldcc.vietbui@lotte.vn>
2025-08-28 11:02:00 +0700

    Add project files.

---
+++ RabbitMq/Consumer/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+            var consumer = new RabbitMQ.Client.Events.EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = System.Text.Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received mesage {0}...", message);
+            };
+
+            channel.BasicConsume("BasicTest", true, consumer);
+            Console.WriteLine(" Press [enter] to exit the Receiver.");
+            Console.ReadLine();
+        }
+    }
+}
